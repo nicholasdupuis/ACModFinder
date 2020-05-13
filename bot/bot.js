6432d4dc3b5099ac796dd0c289f9a06cfca4673c
Nicholas Dupuis <ndupuis08@gmail.com>
2020-05-12 20:56:47 -0500

    Hello world works

--- bot/bot.js
+++ bot/bot.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Logged in! ')
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+let rawdata = fs.readFileSync('cfg/auth.json');
+let discordAuth = JSON.parse(rawdata);
+client.login(discordAuth.token);
